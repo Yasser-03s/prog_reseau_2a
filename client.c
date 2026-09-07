0aec77f058a1ba07dc5f98c7b547517d9e48e3b3
ZAKRI Yasser <Yasser.Zakri@bordeaux-inp.fr>
2026-09-07 09:21:58 +0200

    seance 7/9/2026

--- client.c
+++ client.c
@@ -20,6 +20,44 @@ struct message{
     long int speaker_id;
 };
 
+//fonctions read_on_socket et write_on_socket
+
+
+int read_on_socket(int socketfd, void *ptr, int size){
+
+    int read_bytes = 0;
+    int ret_value = 0;
+    while (read_bytes != size){
+        // CORRECTION : Remplacement de write par read pour lire sur la socket
+        ret_value = read(socketfd, (char *)(ptr) + read_bytes, size - read_bytes);
+        if (ret_value == 0){
+            printf("Disconnected\n");
+            exit(EXIT_FAILURE);
+        }
+        die(ret_value, "reading on socket");
+        read_bytes += ret_value;
+    }
+    return 0;
+}
+
+int write_on_socket(int socketfd, void *ptr, int size){
+    int written_bytes = 0;
+    int ret_value = 0;
+    while (written_bytes != size){
+        ret_value = write(socketfd, (char *)(ptr) + written_bytes, size - written_bytes);
+        if (ret_value == 0){
+            printf("Disconnected\n");
+            exit(EXIT_FAILURE);
+        }
+        die(ret_value, "writing on socket");
+        written_bytes += ret_value;
+    }
+    return 0;
+}
+
+
+
+
 int main(int argc, char* argv[]){
     int client_fd = socket(AF_INET, SOCK_STREAM,0);
     die(client_fd, "socket creation");
@@ -27,12 +65,11 @@ int main(int argc, char* argv[]){
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(1234);
-    //server_addr.sin_addr
+    
     inet_aton("127.0.0.1", &server_addr.sin_addr); //ip config, ip addr
-    //aton ascii to network, pton presentation to network (plus moderne) la diff est pton donne l
+    
     int ret_value;
     ret_value = connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    //?? operation cast (struct sockaddr *)&server_addr
     die(ret_value,"On connecting...");
 
     struct message mymsg;
@@ -41,24 +78,14 @@ int main(int argc, char* argv[]){
     mymsg.size = strlen(data)+1;
     mymsg.speaker_id = 123456789;
 
-    //First send message header
-    //int ret=write(client_fd, &mymsg,sizeof(struct message));
-    //die(ret, "envoi du header");
-    int written_bytes = 0;
-    int to_write = sizeof(struct message);
-    while (written_bytes != to_write){
-        ret_value = write(client_fd, (char *)&mymsg + written_bytes, to_write - written_bytes);
-        if (ret_value == 0){
-            printf("Disconnected\n");
-            exit(EXIT_FAILURE);
-        }
-        die(ret_value, "reading msg header");
-        written_bytes += ret_value;
-    }
+    // COMPLÉTION : Envoi de l'en-tête (header) de la structure message d'abord
+    write_on_socket(client_fd, &mymsg, sizeof(struct message));
 
-    //then send the data itself
-    int ret_val = write(client_fd, data, mymsg.size);
-    die(ret_val,"envoi data");
+    // COMPLÉTION : Envoi des données (payload) juste après
+    write_on_socket(client_fd, data, mymsg.size);
+
+    // COMPLÉTION : Fermeture propre de la socket avant de quitter
+    close(client_fd);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -4,7 +4,7 @@
 #include <sys/socket.h>
 #include <errno.h>
 #include <arpa/inet.h>
-//#include <>
+#include <string.h> // Ajout pour la cohérence
 
 #define BACKLOG 10
 
@@ -21,6 +21,37 @@ void die(int ret_value, const char*msg){
     }
 }
 
+// Inclusions des fonctions demandées
+int read_on_socket(int socketfd, void *ptr, int size){
+    int read_bytes = 0;
+    int ret_value = 0;
+    while (read_bytes != size){
+        ret_value = read(socketfd, (char *)(ptr) + read_bytes, size - read_bytes);
+        if (ret_value == 0){
+            printf("Disconnected\n");
+            exit(EXIT_FAILURE);
+        }
+        die(ret_value, "reading on socket");
+        read_bytes += ret_value;
+    }
+    return 0;
+}
+
+int write_on_socket(int socketfd, void *ptr, int size){
+    int written_bytes = 0;
+    int ret_value = 0;
+    while (written_bytes != size){
+        ret_value = write(socketfd, (char *)(ptr) + written_bytes, size - written_bytes);
+        if (ret_value == 0){
+            printf("Disconnected\n");
+            exit(EXIT_FAILURE);
+        }
+        die(ret_value, "writing on socket");
+        written_bytes += ret_value;
+    }
+    return 0;
+}
+
 
 int main(int argc, char* argv[]){
     int listen_fd = socket(AF_INET,SOCK_STREAM,0);
@@ -32,10 +63,8 @@ int main(int argc, char* argv[]){
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET; 
     server_addr.sin_port = htons(1234);
-    //server_addr.sin_addr
 
     inet_aton("127.0.0.1", &server_addr.sin_addr);
-
 
     int ret_bind = bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     die(ret_bind,"On binding...");
@@ -49,23 +78,32 @@ int main(int argc, char* argv[]){
     socklen_t sizeofaddr = sizeof(client_addr);
     printf("start accepting...\n");
     int client_fd = accept(listen_fd, (struct sockaddr *)&client_addr,&sizeofaddr);
-    if (client_fd != -1){
-        printf("New client accepted, and new socket fd is %d\n", client_fd);
-    }
+    
+    // COMPLÉTION : Utilisation de die pour vérifier l'échec d'accept
+    die(client_fd, "On accepting...");
+    printf("New client accepted, and new socket fd is %d\n", client_fd);
 
-    struct message client_msg = {0}; //je sais pas??
-
-    //receive message size
-    int ret = read(client_fd,&client_msg,sizeof(struct message));
-    die(ret,"reception du header");
+    struct message client_msg = {0}; 
+        
+    // COMPLÉTION : Remplacement du read brut par read_on_socket pour le header
+    int return_val = read_on_socket(client_fd, &client_msg, sizeof(struct message));
     printf("MSG HEADER (%d) (%d) (%ld) \n", client_msg.type, client_msg.size, client_msg.speaker_id);
 
-    // on remplace cela char buf[128] = {0} par:
+    // Allocation dynamique du buffer selon la taille reçue dans le header
     char * data = malloc(sizeof(char) * client_msg.size);
-    //receive message itself
-    int return_val = read(client_fd,data,sizeof(struct message));
-    die(return_val,"reception de data");
-    printf("Le MSG est (%s)\n", data);
-   return 0;
+    if (data == NULL) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // COMPLÉTION / CORRECTION : Utilisation de la bonne fonction et des bonnes variables
+    return_val = read_on_socket(client_fd, data, client_msg.size);
+    printf("Le MSG est (%s)\n", data);
+
+    // Nettoyage et fermeture des ressources
+    free(data);
+    close(client_fd);
+    close(listen_fd);
+
+    return 0;
 }
